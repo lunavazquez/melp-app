c720c5517b05653ef07e6de2739e0c0e5f6bc152
Mextli <luna.vazquezv@gmail.com>
2019-02-25 18:15:41 -0600

    se instala firebase y boostrap al proyecto

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: 'AIzaSyBa8nygAxx-25vLMWGJql98CahU1MZ7jCM',
+  authDomain: 'melp-app.firebaseapp.com',
+  databaseURL: 'https://melp-app.firebaseio.com',
+  projectId: 'melp-app',
+  storageBucket: 'melp-app.appspot.com',
+  messagingSenderId: '324771434568',
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
