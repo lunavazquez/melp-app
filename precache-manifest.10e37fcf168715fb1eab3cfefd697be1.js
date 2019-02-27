4db6e825cae21f80defbc1f32a3ee4ceb3244c6b
Mextli <luna.vazquezv@gmail.com>
2019-02-27 12:26:04 -0600

    Updates

---
+++ precache-manifest.10e37fcf168715fb1eab3cfefd697be1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5154d7d303385a00b3f",
+    "url": "/melp-app/static/js/runtime~main.b5154d7d.js"
+  },
+  {
+    "revision": "1b1283510439a9877eb9",
+    "url": "/melp-app/static/js/main.1b128351.chunk.js"
+  },
+  {
+    "revision": "98736a4f09835f2d5e3b",
+    "url": "/melp-app/static/js/2.98736a4f.chunk.js"
+  },
+  {
+    "revision": "1b1283510439a9877eb9",
+    "url": "/melp-app/static/css/main.54f827f6.chunk.css"
+  },
+  {
+    "revision": "483a136ed0990ca5e488db0e96084e5c",
+    "url": "/melp-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8edc1270a98680df579ac21cd2cb53d2.js"
+  "/melp-app/precache-manifest.10e37fcf168715fb1eab3cfefd697be1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/melp-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
