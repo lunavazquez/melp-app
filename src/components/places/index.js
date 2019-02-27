aa7692f7ee958689172064f48cf60751b9d906b0
Mextli <luna.vazquezv@gmail.com>
2019-02-27 11:20:06 -0600

    ultimos cambios

--- src/components/places/index.js
+++ src/components/places/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './index.module.sass';
 
 const Places = ({ places, handleClick }) => {
-  return places.map(({ id, name }) => {
+  return places.map(({ id, name, rating }) => {
     return (
       <div key={id} onClick={() => handleClick(id)} className={styles.place}>
-        {name}
+        <p>{name}</p>
+        <p>Stars {rating}</p>
       </div>
     );
   });
